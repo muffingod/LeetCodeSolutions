ff252169f51ad9e8316ab58d7af51fcb2a8a5c69
kristof.vogyak <kristof.vogyak@abrisconsult.com>
2023-06-05 08:34:11 +0200

    First working solution for 2. Add Two Numbers

--- 2. Add Two Numbers/main.py
+++ 2. Add Two Numbers/main.py
@@ -13,10 +13,28 @@ class ListNode:
 
 class Solution:
   def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-    print(f'l1: {l1}')
-    print(f'l2: {l2}')
+    sum = 0
+    if(l1):
+      sum += l1.val
+    if l2:
+      sum += l2.val
 
-    return
+    if sum >= 10:
+      sum %= 10
+      if l1 and l1.next:
+        l1.next.val += 1
+      elif l2 and l2.next:
+        l2.next.val += 1
+      else:
+        if not l1:
+          l1 = ListNode(0, ListNode(1))
+        else:
+          l1.next = ListNode(1)
+
+    if((l1 and l1.next) or (l2 and l2.next)):
+        return ListNode(sum, self.addTwoNumbers(l1.next if l1 else None, l2.next if l2 else None))
+    else:
+      return ListNode(sum, None)
 
 def lNL(list: List[int]):
   prevListNode = None
@@ -25,22 +43,33 @@ def lNL(list: List[int]):
     prevListNode = listNode
   return prevListNode
 
+def listNodeToList(ln: ListNode):
+  resultArr = []
+  if(ln.next):
+    resultArr.extend(listNodeToList(ln.next))
+  resultArr.append(ln.val)
+  return resultArr
+
 if __name__ == '__main__':
   solution = Solution()
   """
     Input: l1 = [2,4,3], l2 = [5,6,4]
     Output: [7,0,8]
   """
-  print(solution.addTwoNumbers(lNL([2,4,3]), lNL([5,6,4])))
+  l1 = [2,4,3]
+  l2 = [5,6,4]
+  resNode = solution.addTwoNumbers(lNL(l1), lNL(l2))
+  print(listNodeToList(resNode))
+  #print(listNodeToList(solution.addTwoNumbers(lNL([2,4,3]), lNL([5,6,4]))))
   
   """
     Input: l1 = [0], l2 = [0]
     Output: [0]
   """
-  print(solution.addTwoNumbers(lNL([0]), lNL([0])))
+  print(listNodeToList(solution.addTwoNumbers(lNL([0]), lNL([0]))))
 
   """
     Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
     Output: [8,9,9,9,0,0,0,1]
   """
-  print(solution.addTwoNumbers(lNL([9,9,9,9,9,9,9]), lNL([9,9,9,9])))
+  print(listNodeToList(solution.addTwoNumbers(lNL([9,9,9,9,9,9,9]), lNL([9,9,9,9]))))
